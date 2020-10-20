6e7fc898a742ef0bd80596bf278a67e7fc193710
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2020-10-20 12:08:53 -0300

    Adding store index controller action and jbuilder store index template

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::StoresController < ApplicationController
   before_action :set_store, only: [:show]
 
+  def index 
+    return [] if params[:latitude].nil? || params[:longitude].nil?
+
+    @stores = Store.within(params[:latitude].to_f, params[:longitude].to_f)
+      .sort_by { |store| store.ratings_average }
+      .reverse
+  end
+
   def show
   end
 
